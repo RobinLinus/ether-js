e9256197281d68f30315f946ab8d449ce42dd96f
RobinLinus <robin_woll@capira.de>
2016-12-31 05:22:31 +0100

    Add utils for transaction encoding

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ window.Ether=(function(){
 	ethereumjsTx=require("ethereumjs-tx"),
 	privateToAddress=require("ethereumjs-util").privateToAddress;
 
-	function signTransaction(privateKey,tx){
-		tx=new Buffer(tx,"hex");
-		var r=new ethereumjsTx(privateKey);
-		return r.sign(tx),"0x"+r.serialize().toString("hex")
-	}
+
+
 	function generateKeys(){
 		var privateKey = crypto.getRandomValues(new Uint8Array(32));
 		privateKey = new Buffer(privateKey);
 		return {
-			 privateKey: privateKey.toString("hex"),
-			 publicKey:"0x"+privateToAddress(privateKey).toString("hex")
+			 private: privateKey.toString("hex"),
+			 public:"0x"+privateToAddress(privateKey).toString("hex")
 			}
 	}
 
@@ -22,9 +19,46 @@ window.Ether=(function(){
 		return "0x"+privateToAddress(new Buffer(e,"hex")).toString("hex")
 	}
 
+	function signTransaction(privateKey,tx){
+		privateKey = new Buffer(privateKey,"hex");
+		tx = new ethereumjsTx(tx);
+		tx.sign(privateKey);
+		return "0x"+tx.serialize().toString("hex");
+	}
+
+	function generateTx(txTo, valueWei, nonce, gasPrice, gasLimit, data) {
+		if(!data){
+			data = ""; //is this actually no data and the cheapest transaction? 
+		}
+        return {
+            nonce: sanitizeHex(decimalToHex(nonce)),
+            gasPrice: sanitizeHex(decimalToHex(gasPrice)),
+            gasLimit: sanitizeHex(decimalToHex(gasLimit)),
+            to: sanitizeHex(txTo),
+            value: sanitizeHex(decimalToHex(valueWei)),
+            data: sanitizeHex(data)
+        }
+    }
+
+    function sanitizeHex(hex) {
+        hex = hex.substring(0, 2) == '0x' ? hex.substring(2) : hex;
+        if (hex == "") return "";
+        return '0x' + padLeftEven(hex);
+    }
+
+    function decimalToHex(dec) {
+        return dec.toString(16);
+    }
+
+    function padLeftEven(hex) {
+        hex = hex.length % 2 != 0 ? '0' + hex : hex;
+        return hex;
+    }
+
 	return { 
-		signTransaction:signTransaction,
-	  	generateKeys:generateKeys,
-	  	privateToPublic:privateToPublic
+		generateKeys:generateKeys,
+	  	privateToPublic:privateToPublic,
+		generateTx:generateTx,
+	  	signTransaction:signTransaction
 	  }
 }())
